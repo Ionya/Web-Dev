9a0550c32135ba0241f687c8b6909218d24719f1
Ionya <i_baygazy@kbtu.kz>
2023-02-22 21:26:08 +0600

    Lab 4

---
+++ Lab 4/src/app/products.ts
@@ -0,0 +1,93 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+  rating: number;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUVCIrcep1LCwjfJi4xvOlCQ4BI7hBAjWFHg&usqp=CAU',
+    rating: 3
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLBcevbUJxWqI-xMP_HdnNOKTYR9QmovWw2A&usqp=CAU',
+    rating: 5,
+  },
+  {
+    id: 3,
+    name: 'Jai Phone',
+    price: 299,
+    description: '',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZlL3LNRrTaNA4772R4aLrrnDJZdfNFODTwQ&usqp=CAU',
+    rating: 2,
+  },
+  {
+    id: 4,
+    name: 'Samsung',
+    price: 899,
+    description: '',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2d8IJhzYguQsTJvk2CoS4fjvtSfKRGfchJA&usqp=CAU',
+    rating: 53,
+  },
+  {
+    id: 5,
+    name: 'Huawei',
+    price: 1199,
+    description: '',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTp5pKDIdrtLTY_J2abebP1z8fZk3figigBA&usqp=CAU',
+    rating: 4,
+  },{
+    id: 6,
+    name: 'Nokia',
+    price: 1299,
+    description: '',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKpg9Mx7kVBZr1_lXNM5rvrQOLVn2KSlZXAuly3D_78bB2aIOE0ANCmVFwJN2MX1gC0aU&usqp=CAU',
+    rating: 1,
+  },{
+    id: 7,
+    name: 'LG',
+    price: 5099,
+    description: '',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAETpqT8DqnNqDnjEuLqSDm9UMphkfdbmmZGt-vUoLe1aztD7TVpiILcjbCNQ2GBZ-agg&usqp=CAU',
+    rating: 5,
+  },{
+    id: 8,
+    name: 'Nokia 6300',
+    price: 4299,
+    description: '',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5HunblxVDRwi9kWo6Wn8tCrdAbyX9YEzr0w&usqp=CAU',
+    rating: 5,
+  },{
+    id: 9,
+    name: 'Motorola',
+    price: 3299,
+    description: '',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-2ueemSPx0OW1GrRu1wVz7RS1aUNhxGPEt-yJ1LOlf_exmY5--Sndw5owP9Fuf1Q7vTU&usqp=CAU',
+    rating: 4,
+  },{
+    id: 10,
+    name: 'Nokia 3210',
+    price: 9299,
+    description: '',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjbhq12g9Xm0qxRlr7GtIG9oUJe6Kj7VezpA&usqp=CAU',
+    rating: 2,
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
